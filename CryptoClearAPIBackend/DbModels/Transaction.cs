d42b4bf4d3ed2f5d27caa74a212d3e4c7814e64b
ibBenwah <garret.c.benoit@gmail.com>
2022-11-29 00:28:18 -0500

    added Transactions controller

--- CryptoClearAPIBackend/DbModels/Transaction.cs
+++ CryptoClearAPIBackend/DbModels/Transaction.cs
@@ -1,11 +1,17 @@
-﻿namespace CryptoClearAPIBackend.DbModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CryptoClearAPIBackend.DbModels
 {
     public class Transaction
     {
+        [Key]
         public int Id { get; set; }
+
+        [ForeignKey(nameof(User))]        
         public int UserId { get; set; }
         public DateTime TransactionDate { get; set; }
-        public string CoinSymbol { get; set; }
+        public string CoinId { get; set; }
         public decimal Quantity { get; set; }
         public decimal PurchasePrice { get; set; }
     }
