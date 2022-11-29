26c4efbef42bc6a233a140dba9541e186f6ded60
Brennan <brennan.davis@outlook.com>
2022-11-28 20:43:14 -0500

    Reconfigured Models

---
+++ CryptoClearAPIBackend/DbModels/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoClearAPIBackend.DbModels
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double LiquidCash { get; set; }
+        public List<Transaction>? Transactions { get; set; }
+
+    }
+}
