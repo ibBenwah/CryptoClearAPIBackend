cb9c9c60df4b2608d487169437032d556142bacd
ibBenwah <garret.c.benoit@gmail.com>
2022-11-28 19:56:06 -0500

    Adding controllers and DbModels

---
+++ CryptoClearAPIBackend/Controllers/EventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoClearAPIBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
---
+++ CryptoClearAPIBackend/DbModels/Portfolio.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoClearAPIBackend.DbModels
+{
+    public class Portfolio
+    {
+        public string User { get; set; }
+        public decimal Balance { get; set; }
+        public string Coin { get; set; }
+        public decimal Profitablity  { get; set; }
+        public DateTime Transaction { get; set; }
+    }
+}
+
